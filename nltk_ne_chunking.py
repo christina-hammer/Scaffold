626cd3516c797994e97708388a3a431018f33620
christina-hammer <christinaelizabethhammer@gmail.com>
2017-09-08 13:40:55 -0700

    Update nltk_ne_chunking.py
    output categorized named entities and non-named entity NNP's (proper nouns)

--- nltk_ne_chunking.py
+++ nltk_ne_chunking.py
@@ -6,6 +6,7 @@
 
 import nltk
 import sys
+
 from datetime import datetime
 
 if __name__ == "__main__":
@@ -19,6 +20,7 @@ if __name__ == "__main__":
     
     try:    
         in_file = f.read()
+        f.close()
     except IOError:
         sys.exit("Cannot Read File")    
     
@@ -29,12 +31,31 @@ if __name__ == "__main__":
     tagged_phrases = [nltk.pos_tag(phrase) for phrase in tokenized_phrases]
     ne_chunk_phrases = [nltk.ne_chunk(phrase) for phrase in tagged_phrases]
     
-    #test_dt = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    #test_dt = str("out" + test_dt + ".txt")
-    out_file = open("out.txt", 'w')
+    named_entities = {}
+    NNP_non_ne = set()
     
     for phrase in ne_chunk_phrases:
-        out_file.write(str(phrase)+"\n")
+        for token in phrase:
+            if (type(token) is nltk.tree.Tree):
+                if token.label() not in named_entities:              
+                    named_entities[token.label()] = set()
+                    
+                for t in token:
+                    named_entities[token.label()].add(t)
+            else:
+                if str(token[1]) == "NNP":                    
+                    NNP_non_ne.add(str(token[0]))
+                    
+    
+    test_dt = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    test_dt = str("out" + test_dt + ".txt")
+    out_file = open(test_dt, 'w')
+    
+    for label in named_entities.keys():
+        out_file.write(str(label)+": "+ str(named_entities[label]) + "\n")
+        
+    
+    out_file.write("NNP (non named entities: )" + str(NNP_non_ne))
     
     out_file.close()
     
