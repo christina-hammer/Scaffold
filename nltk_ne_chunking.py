beb9c0e1556d46643bfcc810b7f565f0e9159b30
christina-hammer <christinaelizabethhammer@gmail.com>
2017-09-08 12:10:37 -0700

    Update nltk_ne_chunking.py
    takes in txt file, outputs tagged named enitity chunks in text file

--- nltk_ne_chunking.py
+++ nltk_ne_chunking.py
@@ -1,19 +1,41 @@
 #Christina Hammer
 #Last Edit: 09/08/2017
 
+#Code written using Mucho help from:
+#http://www.nltk.org/book/ch07.html
+
 import nltk
+import sys
+from datetime import datetime
 
 if __name__ == "__main__":
     
-    s = "Today I saw John Green set a garbage can on fire at the Arby's in Charlotte, North Carolina"
-    print("s = " + s + "\n")
+    in_file_name = input('Please enter the name of the text file:')
     
-    s = nltk.word_tokenize(s)
-    print("nltk.word_tokenize(s) = " + str(s) + "\n")
+    try:
+        f = open(in_file_name)
+    except IOError:
+        sys.exit("Cannot Open File")
     
-    s = nltk.pos_tag(s)
-    print("nltk.pos_tag(s) = " + str(s) + "\n")
+    try:    
+        in_file = f.read()
+    except IOError:
+        sys.exit("Cannot Read File")    
     
-    s = nltk.ne_chunk(s)
-    print("nltk.ne_chunk(s) = " + str(s) + "\n")
+    phrases = nltk.sent_tokenize(in_file)
+    
+    
+    tokenized_phrases = [nltk.word_tokenize(phrase) for phrase in phrases]
+    tagged_phrases = [nltk.pos_tag(phrase) for phrase in tokenized_phrases]
+    ne_chunk_phrases = [nltk.ne_chunk(phrase) for phrase in tagged_phrases]
+    
+    #test_dt = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    #test_dt = str("out" + test_dt + ".txt")
+    out_file = open("out.txt", 'w')
+    
+    for phrase in ne_chunk_phrases:
+        out_file.write(str(phrase)+"\n")
+    
+    out_file.close()
+    
     
