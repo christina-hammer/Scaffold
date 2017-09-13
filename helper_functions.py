fbdb85de86b8c614857970bddeca8d410061cf85
christina-hammer <christinaelizabethhammer@gmail.com>
2017-09-13 15:18:04 -0700

    Update helper_functions.py
    moved in more functions from main, wrote json loading into dic logic, incorporated named entity chunking module from nltk to help make phrases

--- helper_functions.py
+++ helper_functions.py
@@ -1,72 +1,92 @@
 #Christina Hammer
-#Last Edit: 09/05/2017
+#Last Edit: 09/13/2017
 #helper_functions.py
 
 from Phrase import *
-from Token import Token
+import nltk
+import json
 
 KEYWORDS = None
 
-##credit to:
-##https://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float
-##works on decimals, ints
-def is_digit_number(s):
+def open_and_split_by_phrase(in_file):
+    
     try:
-        float(s)
-        return True
-    except ValueError:
-        return False 
+        f = open(in_file)
+    except IOError:
+        sys.exit("Cannot Open File")
     
-#this will actually load a json and save it as a dictionay. just populating a small dictionary right now as placeholder
-def load_keywords():
-    # example dictionary: KEYWORDS = {"a.m.": "DT", "p.m.": "DT"}
-    KEYWORDS = {}
-    return
-
-def close_keywords():
-    #close json
-    KEYWORDS = None
-    return
+    try:    
+        input_article = f.read()
+    except IOError:
+        sys.exit("Cannot Read File")
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    phrase_strings = sent_detector.tokenize(input_article.strip())
     
-def check_if_keyword(text):
-    if KEYWORDS == None:
-        load_keywords()
-    return 
+    #list of strings
+    return phrase_strings
+
+def write_to_output_file(of_name, scaffold):
     
-#def is_text_number(s):
-    #s.lower()
-    ##need to add more cases and also deal with hyphen situations thirty-three etc.
-    #num_dic = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "million", "billion", "trillion"}
-    
-    #if s in num_dic:
-        #return True
-    #else:
-        #return False
-    
-##the only times that should be replaced with words are the ones below:
-#def is_time_word(text):
-    #return text in {"midnight", "noon"}
-
-
-#small sample of common prepositions, articles, and coordinating conjunctions
-#to deal with keeping multi-word names like "University of California" as one item
-#likely portal to hell for false positives. We'll see.
-def common_connector(word):
-    return word in {'of','the','in','and', 'for'}
-
-
-def connected_proper_nouns(phrase, first_pn, second_pn):
-    for i in range(first_pn+1, second_pn):
-        if not(common_connector(phrase[i])):
-            return False
-    return True
-
-def string_to_phrase(phrase_string):
-        phrase = Phrase()
-        tokenized_phrase = phrase_string.split()
+    outf = open(of_name, 'w')
+    scaffold_output = scaffold.display()
         
-        for i in range(0, len(tokenized_phrase)):
-            t = Token(tokenized_phrase[i], i)
-            phrase.add_token(t)
+    outf.write(scaffold_output)        
+    outf.close() 
+        
+    return
+
+def tree_to_tuple(ne_token):
+    token_text = ""
+    for t in token:
+        token_text = token_text + str(t[0]) + " "    
+    try:
+        token_text = token_text[:-1]
+    
+    specified_entities = {"GPE", "PERSON"}
+    if token.label() not in specified_entities:
+        return (token_text, "NAMED_ENTITY")
+    
+    return (token_text, token.label())
+
+def phrase_maker(phrase_str):    
+    tokenized_phrase = nltk.word_tokenize(phrase_str)
+    tagged_phrase = nltk.pos_tag(tokenized_phrase)
+    ne_chunk_phrase = nltk.ne_chunk(tagged_phrase)
+    
+    tokens = [] #list of tagged tuples
+    for token in ne_chunk_phrase:
+        if type(token) is nltk.tree.Tree:
+            tokens.append(tree_to_tuple(token))
+        else:
+            if (token[0] in KEYWORDS):
+                token[1] = KEYWORDS[token[0]]
+        tokens.append(token)
+    
+    phrase = Phrase(tokens)    
+    return phrase
+   
+def load_keywords():
+    #write code to load JSON file into dictionary    
+    keyword_json = open("keywords.json")
+    keyword_string = keyword_json.read()
+    KEYWORDS = keyword_string.loads(keyword_string)[0]  
+        
+    return
+
+def value_is_date_time(tokens, index):
+    #non-round hour times
+    if not tokens[index].isdigit():
+        return check_time_format(tokens[index])
             
-        return phrase
+    if not float(tokens[index]).is_integer():
+        return False
+    
+    if 0 < float(tokens[index]) and float(tokens[index]) < 13:
+        if index > (len(tokens) - 2):
+            return False
+        if tokens[index + 1] == "p.m." or tokens[index + 1] == "a.m.":
+            return True        
+    elif int(tokens[index]) > 1000 and int(tokens[index]) < 10000:
+        return True
+    
+    return False
