a2e77147813da04283266ca6d1c2d555bef7b3a2
christina-hammer <christinaelizabethhammer@gmail.com>
2017-10-02 11:53:49 -0700

    debugged and running successfully for basic cases

--- nltk_ne_chunking.py
+++ nltk_ne_chunking.py
@@ -1,5 +1,5 @@
 #Christina Hammer
-#Last Edit: 10/01/2017
+#Last Edit: 10/02/2017
 
 #Code written using help from:
 #http://www.nltk.org/book/ch07.html
@@ -18,21 +18,24 @@ def merge_tokens_and_flatten(ne_chunk_tree, merge_tokens):
     i = 0
     
     while(len(merge_tokens) > 0):
+        
         if (i == merge_tokens[0][0]):
-            r = merge_tokens[0][0]
-            l = merge_tokens[0][1]
+            l = merge_tokens[0][0]
+            r = merge_tokens[0][1]
             merge_text = ""
             merge_tag = "NNP"
             
-            while(l > r):                
-                if(type(ne_chunk_tree[r]) is nltk.tree.Tree):
-                    if ((r == i or r == l) and merge_tag == "NNP"):
+            while(l <= r):  
+                
+                if(type(ne_chunk_tree[l]) is nltk.tree.Tree):
+                    if ((l == i or l == r) and merge_tag == "NNP"):
                         merge_tag = ne_chunk_tree[r].label()
                     for t in ne_chunk_tree[r]:
                         merge_text = merge_text + t[0] + " "
                 else:
-                    merge_text = merge_text + ne_chunk_tree[r][0] + " "
-                r = r + 1
+                    #print(merge_text + " + " + str(ne_chunk_tree[r]))
+                    merge_text = merge_text + ne_chunk_tree[l][0] + " "
+                l = l + 1
             
             ne_chunk_list.append((merge_text, merge_tag))
             merge_text = ""
@@ -42,11 +45,11 @@ def merge_tokens_and_flatten(ne_chunk_tree, merge_tokens):
         else:
             ne_chunk_list.append(ne_chunk_tree[i])
             i = i + 1
-        
-        
-        
-    while(i < len(ne_chunk_tree)):
+    
+    
+    while(i < len(ne_chunk_tree)):        
         ne_chunk_list.append(ne_chunk_tree[i])
+        i = i + 1
     
     return ne_chunk_list
 
@@ -56,10 +59,10 @@ def merge_tokens_and_flatten(ne_chunk_tree, merge_tokens):
 def is_nnp(token):
     if (type(token) is nltk.tree.Tree):
         for t in token:
-            if (t[1] == "NNP" or "NNPS"):
+            if (t[1] == "NNP" or t[1] == "NNPS"):
                 return True
     else:
-        if (token[1] == "NNP" or "NNPS"):
+        if (token[1] == "NNP" or token[1] == "NNPS"):            
             return True
     return False
 
@@ -88,6 +91,41 @@ def find_multi_token_nnp(ne_chunk_tree):
     
     return merge_tokens
 
+def create_output_file(ne_chunk_lists):
+    
+    test_dt = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    test_dt = str("output/out" + test_dt + ".txt")
+    out_file = open(test_dt, 'w') 
+    
+    named_entities = {}
+    pos_occurences = {}        
+    
+    
+    for ne_chunk_list in ne_chunk_lists:
+                              
+        for token in ne_chunk_list:         
+            if (type(token) is nltk.tree.Tree):
+                if token.label() not in named_entities:              
+                    named_entities[token.label()] = set()
+                    
+                for t in token:
+                    named_entities[token.label()].add(t)
+            else:
+                if token[1] not in pos_occurences:                    
+                    pos_occurences[token[1]] = set()
+                pos_occurences[token[1]].add(token[0])
+                    
+    
+    for label in named_entities.keys():
+        out_file.write(str(label)+": "+ str(named_entities[label]) + "\n")
+        
+    for pos in pos_occurences.keys():
+        out_file.write(pos + ": " + str(pos_occurences[pos]) + "\n")
+    
+    out_file.close()    
+    
+    return
+
 if __name__ == "__main__":
     
     in_file_name = input('Please enter the name of the text file:')
@@ -111,38 +149,15 @@ if __name__ == "__main__":
     ne_chunk_trees = [nltk.ne_chunk(phrase) for phrase in tagged_phrases]
     
     
-    test_dt = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    test_dt = str("output/out" + test_dt + ".txt")
-    out_file = open(test_dt, 'w') 
+    ne_chunk_lists = []
     
-    named_entities = {}
-    pos_occurences = {}    
+    for ne_chunk_tree in ne_chunk_trees:        
+        merge_tokens = find_multi_token_nnp(ne_chunk_tree) 
+        ne_chunk_list = merge_tokens_and_flatten(ne_chunk_tree, merge_tokens) 
+        ne_chunk_lists.append(ne_chunk_list)
     
-    for ne_chunk_tree in ne_chunk_trees:
-        merge_tokens= find_multi_token_nnp(ne_chunk_tree)
-        ne_chunk_list = merge_tokens_and_flatten(ne_chunk_tree, merge_tokens)
+    #create_output_file(ne_chunk_lists)
 
-        out_file.write(str(phrase) + "\n")
         
-        for token in ne_chunk_list:         
-            if (type(token) is nltk.tree.Tree):
-                if token.label() not in named_entities:              
-                    named_entities[token.label()] = set()
-                    
-                for t in token:
-                    named_entities[token.label()].add(t)
-            else:
-                if token[1] not in pos_occurences:                    
-                    pos_occurences[token[1]] = set()
-                pos_occurences[token[1]].add(token[0])
-                    
-    
-    for label in named_entities.keys():
-        out_file.write(str(label)+": "+ str(named_entities[label]) + "\n")
-        
-    for pos in pos_occurences.keys():
-        out_file.write(pos + ": " + str(pos_occurences[pos]) + "\n")
-    
-    out_file.close()
     
     
