1fbe47f73b3f313dd169ab6689de5ca7eab81350
christina-hammer <christinaelizabethhammer@gmail.com>
2017-10-31 23:29:50 -0700

    Starting code to build web-app using html and flask

---
+++ PyApp/app.py
@@ -0,0 +1,15 @@
+#Christina Hammer
+#Last Edit: 10/31/2017
+#app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
+
