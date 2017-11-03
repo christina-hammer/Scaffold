90531977a80b5a53eafa62cbd2d107c599dbcdd1
christina-hammer <christinaelizabethhammer@gmail.com>
2017-11-03 02:11:47 -0700

    post method on input form that can take input from text area, process, and display result of below

--- PyApp/app.py
+++ PyApp/app.py
@@ -1,14 +1,23 @@
 #Christina Hammer
-#Last Edit: 10/31/2017
+#Last Edit: 11/03/2017
 #app.py
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+import sample_backend
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template('index.html')
+def initial():
+    return render_template("index.html", result=None)
+
+@app.route("/", methods = ['POST'])
+def process_input():
+    text_ = request.form['article']
+    result_ = text_.upper()
+    return render_template("index.html", result=result_)
 
 if __name__ == "__main__":
     app.run()
---
+++ PyApp/sample_backend.py
@@ -0,0 +1,6 @@
+#Christina Hammer
+#Last Edit: 11/2/2017
+#sample-backend.py
+
+def text_process(text):
+    return text
